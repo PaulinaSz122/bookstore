bfb49220e6aa7179165dbed20497de5c004fd645
Paulina Szczepanska <paulina.szczepanska122@gmail.com>
2020-03-12 19:01:51 +0100

    Initial commit

---
+++ src/main/java/io/github/PaulinaSz122/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.PaulinaSz122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
